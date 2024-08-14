7206294e6225d9c425f1f728f26ed75a4e427e39
徐梦强 <1135349080@qq.com>
2024-08-14 09:13:04 +0800

    feat：引入Pinia

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import { useCounterStore } from './store/counter'
+
+const counterStore = useCounterStore()
+
 </script>
 
 <template>
@@ -11,6 +15,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
+  <el-button @click="counterStore.increment">count++</el-button>
   <HelloWorld msg="Vite + Vue" />
 </template>
 
@@ -21,9 +26,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+import { useCounterStore } from '../store/counter'
+
+const counterStore = useCounterStore()
+
 defineProps<{ msg: string }>()
 
 const count = ref(0)
 </script>
 
 <template>
+  <h1>{{ counterStore.count }}</h1>
+  <h2>{{ counterStore.doubleCount }}</h2>
+
   <h1>{{ msg }}</h1>
 
   <div class="card">
@@ -19,17 +26,12 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+    starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
-      >Vue Docs Scaling up Guide</a
-    >.
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
